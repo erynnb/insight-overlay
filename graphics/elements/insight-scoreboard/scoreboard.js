3232c7709da3c3f74762956670aa2d97312e7811
erynnb <hulkisangry@outlook.com>
2018-07-12 11:49:38 -0400

    reworked scoreboard animations to fix stuck bug that would happen randomly when animations were triggered too quickly in succession

--- graphics/elements/insight-scoreboard/scoreboard.js
+++ graphics/elements/insight-scoreboard/scoreboard.js
@@ -36,26 +36,16 @@
 					type: Boolean,
 					observer: '_enableAttackDefend'
 				},
-				visible: {
+				_visible: {
 					type: Boolean,
-					value: false,
-					observer: '_visibleChanged'
-				},
-				timeline: {
-					type: TimelineLite,
-					readOnly: true,
-					value() {
-						return new TimelineLite({
-							autoRemoveChildren: true
-						});
-					}
+					value: false
 				}
 			};
 		}
 
 		ready() {
 			super.ready();
-			this.initialized = false;
+			this.timeline = new TimelineLite({autoRemoveChildren: true});
 			scores.on('change', newVal => {
 				if (!newVal) {
 					return;
@@ -65,45 +55,55 @@
 				this.blueTeamScore = newVal.blu.score;
 				this.redTeamScore = newVal.red.score;
 			});
-
-			scoreboardVisible.on('change', newVal => {
-				this.visible = newVal;
+			NodeCG.waitForReplicants(scores).then(() => {
+				console.log('scores are ready to use');
+				scoreboardVisible.on('change', newVal => {
+					if (newVal) {
+						this.show();
+					} else {
+						this.hide();
+					}
+				});
 			});
 		}
 
 		_blueTeamNameChanged(newVal, oldVal) {
 			const blueName = this.shadowRoot.querySelector('#blue-name');
-			TweenLite.to(blueName, 0.2, {
+			this.timeline.to(blueName, 0.2, {
 				text: newVal
 			});
 		}
 		_redTeamNameChanged(newVal) {
 			const redName = this.shadowRoot.querySelector('#red-name');
-			TweenLite.to(redName, 0.2, {
+			this.timeline.to(redName, 0.2, {
 				text: newVal
 			});
 		}
 		_enableAttackDefend(newVal) {
 			if (this.visible) {
-				const status = Array.from(this.shadowRoot.querySelectorAll('.attack-defend'));
+				console.log('what', newVal)
+				const adBlue = this.shadowRoot.querySelector('.attack-defend-blue');
+				const adRed = this.shadowRoot.querySelector('.attack-defend-red');
 				const tabs = Array.from(this.shadowRoot.querySelectorAll('.tab'));
 
 				if (newVal === false) {
-					this.timeline.to(status, 0.1, {
+					this.timeline.to(adBlue, 0.1, {
 						className: '-=visible'
 					});
-					this.timeline.fromTo(tabs, 0.2, {
-						className: '+=visible'
-					}, {
+					this.timeline.to(adRed, 0.1, {
+						className: '-=visible'
+					});
+					this.timeline.to(tabs, 0.2, {
 						className: '-=visible'
 					}, 0.2);
 				} else {
-					this.timeline.to(status, 0.3, {
+					this.timeline.to(adBlue, 0.3, {
 						className: '+=visible'
 					}, 0.5);
-					this.timeline.fromTo(tabs, 0.1, {
-						className: '-=visible'
-					}, {
+					this.timeline.to(adRed, 0.3, {
+						className: '+=visible'
+					}, 0.5);
+					this.timeline.to(tabs, 0.1, {
 						className: '+=visible'
 					}, 0);
 				}
@@ -113,25 +113,28 @@
 			const adBlue = this.shadowRoot.querySelector('.attack-defend-blue');
 			const adRed = this.shadowRoot.querySelector('.attack-defend-red');
 			if (this.attackDefend === 'red') {
-				TweenLite.set(adBlue, {
+				this.timeline.set(adBlue, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/shield.png)'
 				});
-				TweenLite.set(adRed, {
+				this.timeline.set(adRed, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/swords.png)'
 				});
 			} else {
-				TweenLite.set(adRed, {
+				this.timeline.set(adRed, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/shield.png)'
 				});
-				TweenLite.set(adBlue, {
+				this.timeline.set(adBlue, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/swords.png)'
 				});
 			}
 		}
-		_visibleChanged(newVal, oldVal) {
-			if ((oldVal === null) && this.initialized) {
+
+		show() {
+			if (this._visible) {
 				return;
 			}
+			this._visible = true;
+
 			const lines = Array.from(this.shadowRoot.querySelectorAll('.line'));
 			const bars = Array.from(this.shadowRoot.querySelectorAll('.bar'));
 			const glows = Array.from(this.shadowRoot.querySelectorAll('.glow'));
@@ -142,146 +145,139 @@
 			const redName = this.shadowRoot.querySelector('#red-name');
 			const adBlue = this.shadowRoot.querySelector('.attack-defend-blue');
 			const adRed = this.shadowRoot.querySelector('.attack-defend-red');
-			const status = Array.from(this.shadowRoot.querySelectorAll('.attack-defend'));
 			const tabs = Array.from(this.shadowRoot.querySelectorAll('.tab'));
 
-			this.initialized = true;
 			if (this.attackDefend === 'red') {
-				TweenLite.set(adBlue, {
+				this.timeline.set(adBlue, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/shield.png)'
 				});
-				TweenLite.set(adRed, {
+				this.timeline.set(adRed, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/swords.png)'
 				});
 			} else {
-				TweenLite.set(adRed, {
+				this.timeline.set(adRed, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/shield.png)'
 				});
-				TweenLite.set(adBlue, {
+				this.timeline.set(adBlue, {
 					backgroundImage: 'url(./elements/insight-scoreboard/img/swords.png)'
 				});
 			}
-
-			if (!oldVal && newVal) {
-				nodecg.playSound('scoreboard-in');
-				this.timeline.fromTo(lines, 0.5, {
-					className: '-=visible'
-				}, {
-					className: '+=visible'
-				}, 0);
-				this.timeline.fromTo(bars, 0.5, {
-					className: '-=visible'
-				}, {
-					className: '+=visible'
-				}, 0.5);
-				this.timeline.fromTo(glows, 0.5, {
-					className: '-=visible'
-				}, {
-					className: '+=visible'
-				}, 0.6);
-				this.timeline.fromTo(logos, 0.5, {
-					className: '-=visible'
-				}, {
-					className: '+=visible'
-				}, 1);
-				this.timeline.fromTo(names, 1, {
-					className: '-=visible'
-				}, {
-					className: '+=visible'
-				}, 1);
-				//	ease: SteppedEase.easeIn
-				if (this.blueTeamName === null) {
-					this.blueTeamName = 'Team A';
-				}
-				this.timeline.fromTo(blueName, 1, {
-					text: ''
-				}, {
-					text: this.blueTeamName,
-					ease: SteppedEase.config(20)
-				}, 1);
-
-				if (this.redTeamName === null) {
-					this.redTeamName = 'Team B';
-				}
-				this.timeline.fromTo(redName, 1, {
-					text: ''
-				}, {
-					text: this.redTeamName,
-					ease: SteppedEase.config(20)
-				}, 1);
-				this.timeline.fromTo(scores, 0.2, {
-					className: '-=visible'
-				}, {
-					className: '+=visible'
-				}, 2);
-
-				if (this.scoreboardMode === true) {
-					this.timeline.fromTo(tabs, 0.4, {
-						className: '-=visible'
-					}, {
-						className: '+=visible'
-					}, 0.45);
-					this.timeline.fromTo(status, 1, {
-						className: '-=visible'
-					}, {
-						className: '+=visible'
-					}, 2.0);
-				}
-			} else if (oldVal && !newVal) {
-				nodecg.playSound('scoreboard-out');
-				this.timeline.fromTo(scores, 0.2, {
-					className: '+=visible'
-				}, {
-					className: '-=visible'
-				}, 0);
-				this.timeline.fromTo(names, 0.5, {
-					className: '+=visible'
-				}, {
-					className: '-=visible'
-				}, 0.2);
-				if (this.blueTeamName === null) {
-					this.blueTeamName = '';
-				}
-				this.timeline.fromTo(blueName, 0.5, {
-					text: this.blueTeamName
-				}, {
-					text: ''
-				}, 0.2);
-				if (this.redTeamName === null) {
-					this.redTeamName = '';
-				}
-				this.timeline.fromTo(redName, 0.5, {
-					text: this.redTeamName
-				}, {
-					text: ''
-				}, 0.2);
-				this.timeline.fromTo(logos, 0.2, {
-					className: '+=visible'
-				}, {
-					className: '-=visible'
-				}, 0.7);
-				this.timeline.fromTo(bars, 0.2, {
-					className: '+=visible'
-				}, {
-					className: '-=visible'
-				}, 0.8);
-				this.timeline.fromTo(glows, 0.2, {
-					className: '+=visible'
-				}, {
-					className: '-=visible'
-				}, 1);
-				this.timeline.fromTo(lines, 0.5, {
-					className: '+=visible'
-				}, {
-					className: '-=visible'
-				}, 1.1);
-				this.timeline.to(tabs, 0.2, {
-					className: '-=visible'
-				}, 0.9);
-				this.timeline.to(status, 0.1, {
-					className: '-=visible'
-				}, 0);
+			nodecg.playSound('scoreboard-in');
+			this.timeline.add('start');
+			this.timeline.to(lines, 0.5, {
+				className: '+=visible'
+			}, 'start');
+			this.timeline.to(bars, 0.5, {
+				className: '+=visible'
+			}, 'start+=0.5');
+			this.timeline.to(glows, 0.5, {
+				className: '-=visible'
+			}, {
+				className: '+=visible'
+			}, 'start+=0.6');
+			this.timeline.to(logos, 0.5, {
+				opacity: 1
+			}, 'start+=1');
+			this.timeline.to(names, 1, {
+				className: '+=visible'
+			}, 'start+=1');
+			//	ease: SteppedEase.easeIn
+			if (this.blueTeamName === null) {
+				this.blueTeamName = 'Team A';
 			}
+			console.log(this.blueTeamName)
+			this.timeline.fromTo(blueName, 1, {
+				text: '',
+				immediateRender:false
+			}, {
+				text: this.blueTeamName,
+				ease: SteppedEase.config(20)
+			}, 'start+=1');
+
+			if (this.redTeamName === null) {
+				this.redTeamName = 'Team B';
+			}
+			this.timeline.fromTo(redName, 1, {
+				text: '',
+				immediateRender:false
+			}, {
+				text: this.redTeamName,
+				ease: SteppedEase.config(20)
+			}, 'start+=1');
+			this.timeline.to(scores, 0.2, {
+				className: '+=visible'
+			}, 'start+=2');
+
+			if (this.scoreboardMode === true) {
+				this.timeline.to(tabs, 0.4, {
+					className: '+=visible'
+				}, 'start+=0.45');
+				this.timeline.to(adBlue, 1, {
+					className: '+=visible'
+				}, 'start+=2');
+				this.timeline.to(adRed, 1,  {
+					className: '+=visible'
+				}, 'start+=2');
+			}
+		}
+		hide() {
+			if (!this._visible) {
+				return;
+			}
+			this._visible = false;
+			const lines = Array.from(this.shadowRoot.querySelectorAll('.line'));
+			const bars = Array.from(this.shadowRoot.querySelectorAll('.bar'));
+			const glows = Array.from(this.shadowRoot.querySelectorAll('.glow'));
+			const logos = Array.from(this.shadowRoot.querySelectorAll('.logo'));
+			const names = Array.from(this.shadowRoot.querySelectorAll('.name'));
+			const scores = Array.from(this.shadowRoot.querySelectorAll('.score'));
+			const blueName = this.shadowRoot.querySelector('#blue-name');
+			const redName = this.shadowRoot.querySelector('#red-name');
+			const adBlue = this.shadowRoot.querySelector('.attack-defend-blue');
+			const adRed = this.shadowRoot.querySelector('.attack-defend-red');
+
+			const tabs = Array.from(this.shadowRoot.querySelectorAll('.tab'));
+			this.timeline.add('start');
+
+			nodecg.playSound('scoreboard-out');
+			this.timeline.to(scores, 0.2, {className: '-=visible'
+			}, 'start');
+			this.timeline.to(names, 0.5, {
+				className: '-=visible'
+			}, 'start+=0.2');
+			if (this.blueTeamName === null) {
+				this.blueTeamName = '';
+			}
+			this.timeline.to(blueName, 0.5, {
+				text: ''
+			}, 'start+=0.2');
+			if (this.redTeamName === null) {
+				this.redTeamName = '';
+			}
+			this.timeline.to(redName, 0.5, {
+				text: ''
+			}, 'start+=0.2');
+			this.timeline.to(logos, 0.2, {
+				opacity: 0
+			}, 'start+=0.7');
+			this.timeline.to(bars, 0.2, {
+				className: '-=visible'
+			}, 'start+=0.8');
+			this.timeline.to(glows, 0.2, {
+				className: '-=visible'
+			}, 'start+=1');
+			this.timeline.to(lines, 0.5, {
+				className: '-=visible'
+			}, 'start+=1.1');
+			this.timeline.to(tabs, 0.2, {
+				className: '-=visible'
+			}, 'start+=0.9');
+			this.timeline.set(adBlue, {
+				className: '-=visible',
+			}, 'start');
+			this.timeline.set(adRed, {
+				className: '-=visible',
+			}, 'start');
 		}
 	}
 
